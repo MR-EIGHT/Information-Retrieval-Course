0c6576785132175ad3fc5b18ca41678b2ecba4ad
Mehrdad HNA <58226760+MR-EIGHT@users.noreply.github.com>
2021-10-08 16:50:54 +0330

    added and method.

--- src/com/Eight/IR/invertedIndex/PostingList.java
+++ src/com/Eight/IR/invertedIndex/PostingList.java
@@ -1,24 +1,66 @@
 package com.Eight.IR.invertedIndex;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class PostingList {
     private List<Integer> docIds = new ArrayList<>();
 
-    public void add(int id){
+    PostingList() {
+
+    }
+
+    PostingList(int... ids) {
+        for (int id : ids) {
+            docIds.add(id);
+        }
+    }
+
+    public static void main(String[] args) {
+        PostingList l1 = new PostingList(1, 3, 5, 8, 9);
+        System.out.println(l1);
+        PostingList l2 = new PostingList(0, 3, 8, 15, 20);
+        System.out.println(l2);
+        System.out.println();
+        System.out.println(l1.and(l2));
+    }
+
+    public void add(int id) {
         docIds.add(id);
     }
 
-    public void sort(){
+    public void sort() {
         Collections.sort(docIds);
     }
 
-    public PostingList and(PostingList list){
-        int i= 0;
-        int j = 0;
-
+    public int size() {
+        return docIds.size();
     }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(docIds.toArray());
+    }
+
+    public PostingList and(PostingList otherList) {
+        PostingList result = new PostingList();
+        int i = 0, j = 0;
+
+        while (i < size() && j < otherList.size()) {
+            int a = docIds.get(i);
+            int b = otherList.docIds.get(j);
+            if (a == b) {
+                result.add(a);
+                i++;
+                j++;
+            } else if (a < b) {
+                i++;
+            } else j++;
+        }
+        return result;
+    }
+
 
 }
